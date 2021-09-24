dd70790f1522f11e642d3c9fdbb7c7d87e15586e
mymafm0405m <mymafm0405@gmail.com>
2021-09-24 14:11:57 +0300

    fix prodution

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDPw6Z2tai2GgsKs-H3jTIVH3ajyklJuiQ',
+    authDomain: 'random-select.firebaseapp.com',
+    databaseURL: 'https://random-select.firebaseio.com',
+    projectId: 'random-select',
+    storageBucket: 'random-select.appspot.com',
+    messagingSenderId: '180163858083',
+    appId: '1:180163858083:web:3abe995cd1f7334d49f916',
+    measurementId: 'G-69B96ZW7EV'
+  }
 };

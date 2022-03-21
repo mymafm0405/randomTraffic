ae2bc2176c81b7543ad13b805234db508840cd2a
Mahmoud <mymafm0405@gmail.com>
2022-03-21 17:17:58 +0300

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyDPw6Z2tai2GgsKs-H3jTIVH3ajyklJuiQ',
-    authDomain: 'random-select.firebaseapp.com',
-    databaseURL: 'https://random-select.firebaseio.com',
-    projectId: 'random-select',
-    storageBucket: 'random-select.appspot.com',
-    messagingSenderId: '180163858083',
-    appId: '1:180163858083:web:3abe995cd1f7334d49f916',
-    measurementId: 'G-69B96ZW7EV'
+    apiKey: "AIzaSyDMmnIA14A4Inr2FFk9q8zOqg4F3CGmrsQ",
+    authDomain: "randomtraffic.firebaseapp.com",
+    projectId: "randomtraffic",
+    storageBucket: "randomtraffic.appspot.com",
+    messagingSenderId: "949312000721",
+    appId: "1:949312000721:web:fb085aa9db3ddde7722bf0",
+    measurementId: "G-VHKTT2J3NZ"
   }
 };
